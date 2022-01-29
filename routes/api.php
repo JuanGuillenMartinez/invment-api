74c68e33ece307f46d6813a2db4e522779727d79
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-01-28 21:39:41 -0600

    Eliminado el middleware de api de las rutas de subcategorias

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('v1/subcategories', SubcategoryController::class)->middleware('api');
+Route::apiResource('v1/subcategories', SubcategoryController::class);
