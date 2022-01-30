10af224e1612eb463107d67b416bc034cd4e286b
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-01-29 20:45:40 -0600

    Añadido grupo de middlewares

--- routes/api.php
+++ routes/api.php
@@ -20,10 +20,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('v1/subcategories', SubcategoryController::class);
-
 Route::post('v1/users/register', [UserController::class, 'register']);
 
 Route::post('v1/users/login', [UserController::class, 'login']);
 
-Route::get('v1/users/show', [UserController::class, 'show'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::apiResource('v1/subcategories', SubcategoryController::class);
+    Route::get('v1/users/show', [UserController::class, 'show']);
+    Route::apiResource('v1/categories', CategoryController::class);
+});
+
